779c811ef5d571dcfb953a585113f16f42ff898a
thebatman1 <mrinmay.mukherjee1@gmail.com>
2018-03-17 01:45:47 +0530

    Added swap function

--- swap.c
+++ swap.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 
+void swap(int *a, int *b) {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
 int main(void) {
 	int a = 10;
 	int b = 20;
 	//Before swap
 	printf("%d %d\n", a, b);
+	swap(&a, &b);
 	//After swap
 	printf("%d %d\n", a, b);
 	return 0;
