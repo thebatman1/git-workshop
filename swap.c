2f26c87ad32bd7872c6a6888d47cda5d00b161ef
thebatman1 <mrinmay.mukherjee1@gmail.com>
2018-03-17 14:25:55 +0530

    Sure

--- swap.c
+++ swap.c
@@ -7,11 +7,7 @@ void swap(int *a, int *b) {
 }
 
 int main(void) {
-<<<<<<< HEAD
 	int a = 200;
-=======
-	int a = 100;
->>>>>>> 8586e29fb69fa420b21087957f2b965422516585
 	int b = 300;
 	//Before swap
 	printf("%d %d\n", a, b);
